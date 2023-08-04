f9a92b0af614c52a690235893674e98afb5d3972
STUDIO 15 <STUDIO 15@DESKTOP-HFOS1SF>
2023-08-04 12:19:54 +0200

    Add project files.

---
+++ InterfaceExercise/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+namespace InterfaceExercise
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MieEmployee mieEmployee = new MieEmployee();   
+            MieStudent student = new MieStudent();
+
+            ((MieRules)mieEmployee).GoHome();
+
+        }
+    }
+    public class MieEmployee : MieRules
+    {
+        void MieRules.GoHome()
+        {
+            Console.WriteLine("17:00");
+        }
+
+        void MieRules.LunchBreak()
+        {
+            Console.WriteLine("12:00");
+        }
+
+        void MieRules.ReportToMie()
+        {
+            Console.WriteLine("08:00");
+        }
+
+        void MieRules.TeaBreak()
+        {
+            Console.WriteLine("10:00");
+        }
+    }
+    public class MieStudent : MieRules
+    {
+        void MieRules.GoHome()
+        {
+            Console.WriteLine("15:30");
+        }
+
+        void MieRules.LunchBreak()
+        {
+            Console.WriteLine("12:00");
+        }
+
+        void MieRules.ReportToMie()
+        {
+            Console.WriteLine("09:00");
+        }
+
+        void MieRules.TeaBreak()
+        {
+            Console.WriteLine("10:00");
+        }
+    }
+    interface MieRules
+    {
+        void ReportToMie();
+        void LunchBreak();
+        void TeaBreak();
+        void GoHome();
+
+
+    }
+
+
+}
